22d82aae8ec31b37d14c25b93749a63b9e0cb26b
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2026-05-23 17:36:12 +0530

    Reformatted the application files with Ruff
    Reformatted the application files with Ruff with all the latest formatting and linting configurations implemented
    
    Signed-off-by: quantum-adrenaline-1 <yogiraj.chitam@gmail.com>

--- src/backend/core/runtime/settings.py
+++ src/backend/core/runtime/settings.py
@@ -5,16 +5,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class AppSettings(BaseSettings):
     model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore"
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore'
     )
 
     DEBUG: bool = True
 
-    SECRET_KEY: str = ""
+    SECRET_KEY: str = ''
 
-    DJANGO_SETTINGS_MODULE: str = "config.settings.local"
+    DJANGO_SETTINGS_MODULE: str = 'config.settings.local'
 
 
 settings: AppSettings = AppSettings()
